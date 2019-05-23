d85d30a36149fd167d48274c68a7b5e3bdc3563e
Miguel <gannasong@gmail.com>
2019-05-23 23:07:30 +0800

    add: init base controller

---
+++ GNTwitterProfile/GNViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GNViewController.swift
+//  GNTwitterProfile
+//
+//  Created by SUNG HAO LIN on 2019/5/23.
+//  Copyright © 2019 GanNaSong. All rights reserved.
+//
+
+import UIKit
+
+class GNViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .yellow
+  }
+
+}
